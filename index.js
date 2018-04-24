148725f33962f286c1bae3a96157fe65c36f6698
Aliz Nagy <aliznagy@Alizs-MBP.lan>
2018-04-24 22:40:39 +0200

    Hello World

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-console.log('hello world');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
 

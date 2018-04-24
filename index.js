6e737c8c6e8696d62ae7ad8338582e4b2ab1427f
Aliz Nagy <aliznagy@Alizs-MBP.lan>
2018-04-24 22:56:53 +0200

    heroku sample

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.render('index');
+});
+
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
 
